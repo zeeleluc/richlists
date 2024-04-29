e1717c1064bf8f6b283b8fb12413ba30ad38d240
Luc Zeelenberg <lucaszeelenberg@gmail.com>
2024-04-29 19:53:30 -0400

    apply wallet ignore logic also when fetching real time from tables instead of cache

--- src/App/Action/Actions/RichList.php
+++ src/App/Action/Actions/RichList.php
@@ -23,6 +23,19 @@ class RichList extends BaseAction
             $countsPerWallet = $service->getCountsPerWalletFromCache();
             if (!$countsPerWallet) {
                 $countsPerWallet = $service->getCountsPerWallet();
+
+                // we prefer to return always the cache, where the unwanted wallets are already respected.
+                // however after a new release caches are deleted, and you must wait max. 15 minutes for the nex cache.
+                // symlink a folder could solve this problem, but for now we do it simply like this.
+                $unwantedWallets = env('WALLETS_IGNORE_' . strtoupper($project));
+                if ($unwantedWallets) {
+                    $unwantedWallets = explode(',', $unwantedWallets);
+                    foreach ($unwantedWallets as $unwantedWallet) {
+                        if (array_key_exists($unwantedWallet, $countsPerWallet)) {
+                            unset($countsPerWallet[$unwantedWallet]);
+                        }
+                    }
+                }
             }
         } catch (\Exception $e) {
             abort('RichList for ' . $projectName . ' almost ready, try again later.', 'danger');

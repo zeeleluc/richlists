448f82a8cdaf68da43aec9d79ebbe8cef6b34b0d
Luc Zeelenberg <lucaszeelenberg@gmail.com>
2024-05-01 00:26:58 -0400

    move error logic to index

--- index.php
+++ index.php
@@ -3,6 +3,14 @@ include_once 'preload.php';
 include_once 'autoloader.php';
 include_once 'utilities.php';
 
+if (env('ENV') === 'local') {
+    error_reporting(E_ALL ^ E_DEPRECATED ^ E_WARNING);
+    ini_set('display_errors', 'On');
+} else {
+    error_reporting(0);
+    ini_set('display_errors', 'Off');
+}
+
 try {
     // Initialize the application.
     $initialize = new \App\Initialize();
--- preload.php
+++ preload.php
@@ -1,13 +1,5 @@
 <?php
 session_start();
 
-if (env('ENV') === 'local') {
-    error_reporting(E_ALL ^ E_DEPRECATED ^ E_WARNING);
-    ini_set('display_errors', 'On');
-} else {
-    error_reporting(0);
-    ini_set('display_errors', 'Off');
-}
-
 const DS = DIRECTORY_SEPARATOR;
 const ROOT = __DIR__;

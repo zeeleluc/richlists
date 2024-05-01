8e06aeacde16fd84b4d2aa7e275a9d710e9dd7fb
Luc Zeelenberg <lucaszeelenberg@gmail.com>
2024-05-01 00:24:08 -0400

    only show errors on screen on local

--- preload.php
+++ preload.php
@@ -1,8 +1,13 @@
 <?php
 session_start();
 
-error_reporting(E_ALL ^ E_DEPRECATED ^ E_WARNING);
-ini_set('display_errors', 'On');
+if (env('ENV') === 'local') {
+    error_reporting(E_ALL ^ E_DEPRECATED ^ E_WARNING);
+    ini_set('display_errors', 'On');
+} else {
+    error_reporting(0);
+    ini_set('display_errors', 'Off');
+}
 
 const DS = DIRECTORY_SEPARATOR;
 const ROOT = __DIR__;
